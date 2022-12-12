64b068fa7966e8aa096a53c4baa7fa0309c521c7
Malwina <yellowindmills@gmail.com>
2022-12-12 15:57:02 +0100

    fixed check

--- js/main.js
+++ js/main.js
@@ -73,6 +73,13 @@ window.onload = () => {
    function RenderPricingPage(params) {
     document.querySelector('main').innerHTML =
     `
+    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+      <symbol id="check" viewBox="0 0 16 16">
+        <title>Check</title>
+        <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+      </symbol>
+    </svg>
+    
 <div class="container py-3">
     <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
       <h1 class="display-4 fw-normal">Check Out Our Pricing Programs!</h1>
